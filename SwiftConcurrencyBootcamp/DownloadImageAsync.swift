078bd965e92dbf8b10ca041503b17322b4dfd776
HaoYun <haoyun0317@gmail.com>
2024-12-12 10:20:12 +0800

    [Faature]Do-catch Try Throws

---
+++ SwiftConcurrencyBootcamp/DownloadImageAsync.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadImageAsync.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by  謝皓昀 on 2024/12/12.
+//
+
+import SwiftUI
+
+struct DownloadImageAsync: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    DownloadImageAsync()
+}
